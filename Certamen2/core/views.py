c458ca542ba0bf9e5209ac7f71701eccd4478ba0
DiegoHidalgoUSM <127759412+DiegoHidalgoUSM@users.noreply.github.com>
2023-06-08 23:00:16 -0400

    Add files via upload

---
+++ Certamen2/core/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from core.models import Comunicado, Categoria
+
+def index(request):
+    nivelPOST = request.POST.get('nivel', None)
+    categoriaPOST= request.POST.get('categoria', None)
+
+    comunicados = Comunicado.objects.all()
+    comunicados = Comunicado.objects.order_by('-fecha_envio')
+    categorias = Categoria.objects.all()
+
+    if nivelPOST:
+        comunicados = comunicados.filter(nivel=nivelPOST)
+    
+    if categoriaPOST:
+        comunicados = comunicados.filter(categoria_id=categoriaPOST)
+    
+    data = {
+        'comunicados': comunicados,
+        'categorias': categorias
+    }
+
+    return render(request, 'core/index.html', data)
+
